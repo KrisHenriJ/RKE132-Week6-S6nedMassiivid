f6a3b2f5af2692f065c6c012ca8ba65f069cb5b7
KrisHenriJ <kris.henri.jarvi@tktk.ee>
2023-11-10 20:07:07 +0200

    S6nedMassiivid

---
+++ RandomElement/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "fish" };
+Random rnd = new Random();
+
+int RandomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat{snacks[RandomIndex]}.");
+
+
